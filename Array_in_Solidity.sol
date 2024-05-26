0fb68308a361d442241dcb16cb89a90c847102b0
HARSH KUMAR YADAV <116312157+HARSH-YADAV-GLITCH@users.noreply.github.com>
2024-05-26 16:59:21 +0530

    Create Array_in_Solidity.sol
    This is a simple example of Array and array operations in solidity.

---
+++ Array_in_Solidity.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.7;
+contract Array {
+    uint[] public nums = [1,2,3,4,5];
+    uint[3] public numsFixed= [3,2,1];
+
+function example() external {
+    nums.push(6);
+    uint x = nums[1];
+    nums[2]= 69;
+    delete nums[1];
+    nums.pop();
+    uint len = nums.length;
+    uint [] memory a = new uint[](5);
+    a[1]=123;
+}
+function returnArray() external view returns (uint[] memory ){
+    return nums;
+}
+}
